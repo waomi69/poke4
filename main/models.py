676d5be90da469a6a225714daafb49988e9ed5b3
IlyaKul <ikuldarev@mail.ru>
2023-10-26 14:42:13 +0500

    First commit

---
+++ main/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django import forms
+
+
+class FightForm(forms.Form):
+    user_number = forms.IntegerField(
+        label='Ваша атака (от 1 до 10)',
+        min_value=1,
+        max_value=10,
+        required=True,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+
+
+class FastFightForm(forms.Form):
+
+    pass
+
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(label="Введите ваш адрес электронной почты, чтобы получить результат боя",
+                             required=True,
+                             widget=forms.TextInput(
+                                 attrs={'class': 'form-control', 'placeholder': 'Ваш адрес электронной почты'}))
+
+
+class Fight(models.Model):
+    date = models.DateTimeField()
+    round_count = models.IntegerField()
+    pokemon_name = models.CharField(max_length=30)
+    enemy_name = models.CharField(max_length=30)
+    result = models.BooleanField()
---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('pokemon/<pokemon_name>', views.pokemon_detail, name='pokemon_detail'),
+    path('fight/<pokemon_name>', views.fight, name='fight'),
+    path('api/pokemon/random', views.pokemon_random, name='pokemon_random'),
+    path('api/pokemon/list/', views.get_pokemon_list, name='pokemon_list'),
+    path('api/pokemon/<pokemon_id>/', views.get_pokemon_id, name='pokemon_id'),
+    path('api/fight/', views.get_fight_info, name='fight_info'),
+    path('api/fight/<int:number>/', views.update_pokemon_data, name='update_pokemon_data'),
+    path('api/fight/fast/', views.get_fast_fight_result, name='fast_fight_result'),
+]
